3137820c8e2668353cc77ff90b9f83d579384725
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-09 13:03:15 -0500

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
+    # Remember that all methods use "self." We'll elaborate on this in a moment.
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt'):
+        self.name = name
+        self.breed = breed
+
+fido = Dog('Fido', "golden")
+print(fido.name, fido.breed)
+
+
+# class Dog:
+#     def __init__(self, name, favorite_toy='Any'):
+#         self.name = name
+#         self.favorite_toy = favorite_toy
+
+#     def bark(self):
+#         print("Woof!")
+
+#     def get_adopted(self, owner_name):
+#         self.owner = owner_name
+#     def go_walk(self, walker_name):
+#         self.walker = walker_name
+    
+# fido = Dog('Fido', 'Doll')
+# fido.get_adopted('Tyler')
+# print(fido.owner)
+# print(fido.favorite_toy)
+# fido.go_walk('YUki')
+# print(fido.walker)
+# snoopy=Dog('Snoopy')
